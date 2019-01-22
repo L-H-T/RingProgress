0a4b4a7e5609160b135a6e716ade8126328e6b29
L-H-T <lht0825gl@gmail.com>
2019-01-22 17:43:10 +0800

    Initial Commit

---
+++ CircleProgress/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircleProgress
+//
+//  Created by L-H-T on 2019/1/22.
+//  Copyright © 2019 L-H-T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircleProgress/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircleProgress
+//
+//  Created by L-H-T on 2019/1/22.
+//  Copyright © 2019 L-H-T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
